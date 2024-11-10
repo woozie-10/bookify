4deebadbe2ac1548397b4ea2b659b588e83015d9
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-11-10 22:26:52 +0200

    updated global urls

--- bookifyapp/urls.py
+++ bookifyapp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include("bookify.urls")),
     path('accounts/', include('accounts.urls')),
-    path("", include("bookify.urls"))
+ 
 ]
