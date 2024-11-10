d89383cbd5f4645ccdf950b882cc93472ac49a50
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-11-10 22:24:55 +0200

    added views and urls for accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='user_register'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
 ]
