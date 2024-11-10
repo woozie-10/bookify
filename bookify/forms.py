37cb981e4916d7cd6e66ba0a963fb18f7a4db7ae
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-11-10 22:25:28 +0200

    added models and forms for book

---
+++ bookify/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'author', 'description', 'file', 'cover']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Book Name', 'class': 'form-input', 'required': 'true'}),
+            'author': forms.TextInput(attrs={'placeholder': 'Author', 'required': 'true', 'class': 'form-input',}),
+            'description': forms.Textarea(attrs={'placeholder': 'Description', 'rows': 6, 'required': 'true', 'class': 'form-input',}),
+            'file': forms.ClearableFileInput(attrs={'accept': 'application/pdf, .epub, .mobi', 'class': 'file-input', 'required': 'true', 'class': 'file-input'}),
+            'cover': forms.ClearableFileInput(attrs={'accept': 'image/*', 'class': 'file-input', 'required': 'true','class': 'file-input'}),
+        }
--- bookify/models.py
+++ bookify/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Book(models.Model):
+    uploader = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='books')
+    file = models.FileField(upload_to='books/')
+    name = models.CharField(max_length=80)
+    author = models.CharField(max_length=60)
+    cover = models.FileField(upload_to='covers/')
+    description = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
+    
