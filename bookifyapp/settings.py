57c45bb563242ce157dd3af3821b7eb9797cd669
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-11-10 22:23:10 +0200

    added user model and changed settings

--- bookifyapp/settings.py
+++ bookifyapp/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
