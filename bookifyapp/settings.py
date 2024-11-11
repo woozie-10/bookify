524eb38e0e567ea4745a5a5e45e22d6251d15509
woozie-10 <deriuhin.mykhailo@gmail.com>
2024-11-11 20:06:52 +0200

    small fix

--- bookifyapp/settings.py
+++ bookifyapp/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-n0u%wq5+)#ko91g-ygqf)**%)75xl_3_8jpot6(+gxg7582(%2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
